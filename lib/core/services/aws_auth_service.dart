8e3139f608771e7adedb000b749b97ad4b094bbd
Kunal Singh <157101013+kunaldrafts25@users.noreply.github.com>
2026-09-25 18:27:39 +0530

    fix(auth): request Cognito user-admin scope for session bootstrap

--- lib/core/services/aws_auth_service.dart
+++ lib/core/services/aws_auth_service.dart
@@ -497,7 +497,7 @@ class AwsAuthService {
         'response_type': 'code',
         'client_id': clientId,
         'redirect_uri': _cognitoRedirectUri,
-        'scope': 'openid email profile',
+        'scope': 'openid email profile aws.cognito.signin.user.admin',
         'state': state,
         'code_challenge': challenge,
         'code_challenge_method': 'S256',
